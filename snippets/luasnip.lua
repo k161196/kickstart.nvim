7d4eccb86db1d5f4631e11e3ffda5724de94b065
Kiran Yadav <kiranyadav161196@gmail.com>
2024-05-11 14:34:39 +0530

    added snippts

---
+++ snippets/luasnip.lua
@@ -0,0 +1,19 @@
+local ls = require 'luasnip'
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+ls.add_snippets('lua', {
+  s('hello', {
+    t 'print("hello ',
+    i(1),
+    t ' world")',
+  }),
+
+  s('if', {
+    t 'if ',
+    i(1, 'true'),
+    t ' then ',
+    i(2),
+    t ' end',
+  }),
+})
