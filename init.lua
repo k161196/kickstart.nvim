f3866264b4cba729c437ba2293182e785c00f81f
Kiran Yadav <kiranyadav161196@gmail.com>
2024-08-14 22:34:04 +0530

    changes

--- init.lua
+++ init.lua
@@ -300,20 +300,20 @@ require('lazy').setup({
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-      { "<leader>c", group = "[C]ode" },
-    { "<leader>c_", hidden = true },
-    { "<leader>d", group = "[D]ocument" },
-    { "<leader>d_", hidden = true },
-
-    { "<leader>r", group = "[R]ename" },
-    { "<leader>r_", hidden = true },
-    { "<leader>s", group = "[S]earch" },
-    { "<leader>s_", hidden = true },
-    { "<leader>t", group = "[T]oggle" },
-    { "<leader>t_", hidden = true },
-    { "<leader>w", group = "[W]orkspace" },
-    { "<leader>w_", hidden = true },
+    --   require('which-key').register {
+    --   { "<leader>c", group = "[C]ode" },
+    -- { "<leader>c_", hidden = true },
+    -- { "<leader>d", group = "[D]ocument" },
+    -- { "<leader>d_", hidden = true },
+    --
+    -- { "<leader>r", group = "[R]ename" },
+    -- { "<leader>r_", hidden = true },
+    -- { "<leader>s", group = "[S]earch" },
+    -- { "<leader>s_", hidden = true },
+    -- { "<leader>t", group = "[T]oggle" },
+    -- { "<leader>t_", hidden = true },
+    -- { "<leader>w", group = "[W]orkspace" },
+    -- { "<leader>w_", hidden = true },
 
         -- ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
         -- ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
@@ -322,16 +322,16 @@ require('lazy').setup({
         -- ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         -- ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
         -- ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-      }
+      -- }
       -- visual  { import = "lazyvim.plugins.extras.test.core" },mode
-      require('which-key').register(
-
-     -- {
-     --   ['<leader>h'] = { 'Git [H]unk' },
-     -- }, { mode = 'v' }
-      { "<leader>h", desc = "Git [H]unk", mode = "v" }
-
-      )
+     --  require('which-key').register(
+     --
+     -- -- {
+     -- --   ['<leader>h'] = { 'Git [H]unk' },
+     -- -- }, { mode = 'v' }
+     --  { "<leader>h", desc = "Git [H]unk", mode = "v" }
+     --
+     --  )
     end,
   },
 
